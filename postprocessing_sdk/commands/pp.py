426388deedba2cfd5260af5978e841bf3eded928
Artem Zenkov <azenkov@isgneuro.com>
2022-11-30 21:21:48 +0700

    add platform_envs dictionary support

--- postprocessing_sdk/commands/pp.py
+++ postprocessing_sdk/commands/pp.py
@@ -62,11 +62,16 @@ class Command(BaseCommand):
             '--storage',
             help='Directory for dataframes storage', nargs='?', default=None,
         )
+        parser.add_argument(
+            '--user',
+            help='User unique identifier', nargs='?', default=None,
+        )
 
     def handle(self, *args, **options):
         otl_query = options['otl_query']
         storage = options['storage'] or 'storage'
         commands_dir = options['commands_dir']
+        user_id = options['user'] or 'admin'
         Path(storage).mkdir(parents=True, exist_ok=True)
 
         if not commands_dir:
@@ -76,13 +81,13 @@ class Command(BaseCommand):
                 commands_dir = POST_PROC_COMMAND_DIR
 
         self._create_command_executor(storage, commands_dir)
-
+        platform_envs = {'user_guid': user_id}
         if otl_query is None:
-            self.repl(storage, commands_dir)
+            self.repl(storage, commands_dir, platform_envs)
         else:
-            self.run_otl(otl_query, storage, commands_dir)
+            self.run_otl(otl_query, storage, commands_dir, platform_envs)
 
-    def repl(self, storage, commands_dir):
+    def repl(self, storage, commands_dir, platform_envs=None):
         print(f'Storage directory is {Path(storage).resolve()}')
         print(f'Commmands directory is {Path(commands_dir).resolve()}')
         completer = Completer(self.command_executor)
@@ -95,7 +100,7 @@ class Command(BaseCommand):
             if otl_query == '\?' or otl_query == 'help':
                 self.print_help()
             else:
-                self.run_otl(otl_query, storage, commands_dir)
+                self.run_otl(otl_query, storage, commands_dir, platform_envs)
 
     def _create_command_executor(self, storage, commands_dir):
         storages = {
@@ -127,7 +132,7 @@ class Command(BaseCommand):
         command_help.append(f"\t\tuse_timewindow: {command_syntax_dict['use_timewindow']}")
         return '\n'.join(command_help)
 
-    def run_otl(self, otl_query, storage, commands_dir):
+    def run_otl(self, otl_query, storage, commands_dir, platform_envs=None):
         command_executor = self.command_executor
         # set dev storage for all user commands
         for command_class in command_executor.command_classes.keys():
@@ -170,7 +175,7 @@ class Command(BaseCommand):
                     commands
                 )
             )
-            df = command_executor.execute(commands)
+            df = command_executor.execute(commands, platform_envs)
             print(df)
         except Exception as err:
             tb = traceback.format_exc()
