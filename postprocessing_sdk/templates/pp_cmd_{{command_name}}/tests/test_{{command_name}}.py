0f5158c085299467ab6e747f1190094d1d0e7c3e
Nikita Serditov <nserditov@isgneuro.com>
2023-10-26 14:38:55 +0300

    minor typo fixes to make default test run

--- postprocessing_sdk/templates/pp_cmd_{{command_name}}/tests/test_{{command_name}}.py
+++ postprocessing_sdk/templates/pp_cmd_{{command_name}}/tests/test_{{command_name}}.py
@@ -2,6 +2,7 @@
 import os
 import pandas as pd
 from unittest import TestCase
+from sys import prefix
 
 from postprocessing_sdk.commands.pp import Command
 
@@ -12,17 +13,16 @@ class TestCommand(TestCase):
 
     def setUp(self):
         self.command = Command()
-        commands_dir = os.path.join(os.path.dirname(os.getcwd()),
-                                    'venv/lib/python3.9/site-packages/postprocessing_sdk/pp_cmd')
+        commands_dir = os.path.join(prefix, 'lib/python3.9/site-packages/postprocessing_sdk/pp_cmd')
         self.command._create_command_executor(storage='', commands_dir=commands_dir)
-        parent_dir = os.path.dirname(os.getcwd())
+        parent_dir = os.path.dirname(os.path.dirname(__file__))
         # add current command to self.command with _import_user_commands()
         self.command.command_executor.command_classes.update(
             self.command.command_executor._import_user_commands(commands_directory=parent_dir, follow_links=True))
 
     def test_{{command_name}}_command(self):
         # enter sample dataframe
-        sample = pd.DataFrame()
+        sample = pd.DataFrame([[1, 2, 3], [2, 3, 4]], columns=["a", "b", "c"])
         # create otl query that should return the same dataframe as you have in sample
         otl_query = '| {{command_name}} first_positional_argument'
         # calculate otl query with postprocessing
